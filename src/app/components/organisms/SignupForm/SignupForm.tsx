ac232e7ba91519f6dfcfaba4eb58b44989eb9873
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-16 16:56:41 +0900

    React Hook Formをインストールし、ユーザー登録処理を実装 (#1)

--- src/app/components/organisms/SignupForm/SignupForm.tsx
+++ src/app/components/organisms/SignupForm/SignupForm.tsx
@@ -1,32 +1,80 @@
 "use client";
 import type { FC } from "react";
+import type { SubmitHandler } from "react-hook-form";
+import type { User } from "@/app/types/User";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export const SignupForm: FC = () => {
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    getValues,
+  } = useForm<User>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    }
+  });
+
+  const onSubmit: SubmitHandler<User> = async (data) => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/users`, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: data.name, email: data.email, password: data.password, password_confirmation: data.password_confirmation }),
+      });
+      if (!res.ok) {
+        const err = await res.json();
+        throw new Error(err.message);
+      }
+      router.replace("/");
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+  
   return (
-    <form className="h-full flex items-center justify-center text-center">
+    <form onSubmit={handleSubmit(onSubmit)} className="h-full flex items-center justify-center text-center">
       <fieldset className="border rounded-2xl grow">
         <legend className="px-2">登録</legend>
+        <p className="text-red-500">{error}</p>
         <label>
           名前
-          <input type="text" name="name" id="name" className="border block mx-auto" />
+          <input type="text" {...register("name", { required: "必須項目です。", maxLength: { value: 10, message: "10文字以内で入力してください。" }})} name="name" id="name" className="border block mx-auto" />
         </label>
+        <p className="text-red-500">{errors.name?.message}</p>
 
         <label>
           メールアドレス
-          <input type="email" name="email" id="email" className="border block mx-auto" />
+          <input type="email" {...register("email", { required: "必須項目です。" })} name="email" id="email" className="border block mx-auto" />
         </label>
+        <p className="text-red-500">{errors.email?.message}</p>
 
         <label>
           パスワード
-          <input type="password" name="password" id="password" className="border block mx-auto" />
+          <input type="password" {...register("password", { required: "必須項目です。", minLength: { value: 6, message: "6文字以上、30文字以内で入力してください" }, maxLength: { value: 30, message: "6文字以上、30文字以内で入力してください" } })} name="password" id="password" className="border block mx-auto" />
         </label>
+        <p className="text-red-500">{errors.password?.message}</p>
 
         <label>
           パスワード確認
-          <input type="password" name="password_confirmation" id="password_confirmation" className="border block mx-auto" />
+          <input type="password" {...register("password_confirmation", { required: "必須項目です。", validate: { message: (value) => value === getValues("password") || "パスワードが一致しません" }})} name="password_confirmation" id="password_confirmation" className="border block mx-auto" />
         </label>
+        <p className="text-red-500">{errors.password_confirmation?.message}</p>
 
-        <button>登録</button>
+        <button disabled={isSubmitting}>登録</button>
       </fieldset>
     </form>
   );
