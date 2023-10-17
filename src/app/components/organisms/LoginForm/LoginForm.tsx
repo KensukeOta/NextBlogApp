47bb9f38005afb22d1774b475304982de708ff69
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-17 20:58:56 +0900

    ログイン処理を実装し、ログインユーザー名をトップページに表示 (#3)

--- src/app/components/organisms/LoginForm/LoginForm.tsx
+++ src/app/components/organisms/LoginForm/LoginForm.tsx
@@ -1,23 +1,71 @@
 "use client";
 import type { FC } from "react";
+import type { SubmitHandler } from "react-hook-form";
+import type { User } from "@/app/types/User";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Cookies from "js-cookie";
 
 export const LoginForm: FC = () => {
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<User>({
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  });
+
+  const onSubmit: SubmitHandler<User> = async (data) => {
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sanctum/csrf-cookie`, {
+        credentials: "include",
+      });
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/users/login`, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN") ?? "",
+        },
+        body: JSON.stringify({ email: data.email, password: data.password }),
+        credentials: "include",
+      });
+      if (!res.ok) {
+        const err = await res.json();
+        throw new Error(err.message);
+      }
+      router.replace("/");
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
   return (
-    <form className="h-full flex items-center justify-center text-center">
+    <form onSubmit={handleSubmit(onSubmit)} className="h-full flex items-center justify-center text-center">
       <fieldset className="border rounded-2xl grow">
         <legend className="px-2">ログイン</legend>
-
+        <p className="text-red-500">{error}</p>
         <label>
           メールアドレス
-          <input type="email"　name="email" id="email" className="border block mx-auto" />
+          <input type="email" {...register("email", { required: "必須項目です" })} name="email" id="email" className="border block mx-auto" />
         </label>
+        <p className="text-red-500">{errors.email?.message}</p>
 
         <label>
           パスワード
-          <input type="password" name="password" id="password" className="border block mx-auto" />
+          <input type="password" {...register("password", { required: "必須項目です" })} name="password" id="password" className="border block mx-auto" />
         </label>
+        <p className="text-red-500">{errors.password?.message}</p>
 
-        <button>ログイン</button>
+        <button disabled={isSubmitting}>ログイン</button>
       </fieldset>
     </form>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
-export default function TopPage() {
+import { headers } from "next/headers";
+
+const getAuthUser = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/user`, {
+      headers: {
+        Accept: "application/json",
+        Cookie: headers().get("cookie") ?? "",
+        referer: headers().get("referer") ?? ""
+      },
+      cache: "no-store",
+      credentials: "include"
+    });
+    if (!res.ok) {
+      const errors = await res.json();
+      throw new Error(errors.message);
+    }
+    const data = await res.json();
+    return data.authUser;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function TopPage() {
+  const authUser = await getAuthUser();
+  console.log(authUser);
+
   return (
     <>
       <h1 className="font-bold">トップページ</h1>
+      <p>Welcome! {authUser ? authUser.name : "stranger"}</p>
     </>
   );
 }
