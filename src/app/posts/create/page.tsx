1beb4fd44576cc7275f071a49a5c8914121824ef
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-19 17:36:17 +0900

    未ログイン時に記事投稿ページに遷移しようとしたら、ログインページへリダイレクトするよう実装 (#5)

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 import { getAuthUser } from "@/app/lib/getAuthUser";
 import { PostForm } from "@/app/components/organisms/PostForm";
-import { User } from "@/app/types/User";
 
 export const metadata: Metadata = {
   title: "記事投稿フォーム - NextBlogApp",
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 
 export default async function PostCreatePage() {
   const authUser = await getAuthUser();
+  if (!authUser) {
+    redirect("/login");
+  }
 
   return (
     <PostForm authUser = {authUser} />
