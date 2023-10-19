7784e2490857a4fe7a1c067512183f6c50d51b75
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-19 23:43:01 +0900

    記事一覧表示処理を実装 (#7)

--- src/app/components/organisms/PostItem/PostItem.tsx
+++ src/app/components/organisms/PostItem/PostItem.tsx
@@ -4,9 +4,9 @@ import type { PostProps } from "@/app/types/PostProps";
 export const PostItem: FC<PostProps> = ({ post }) => {
   return (
     <article className="border p-2">
-      <h2 className="font-bold">Test</h2>
+      <h2 className="font-bold">{post.title}</h2>
       <nav className="flex justify-between">
-        by kensuke
+        by {post.user.name}
       </nav>
     </article>
   );
