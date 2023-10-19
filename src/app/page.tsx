b5678953a6a557002c1671d986d37b653a190288
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2023-10-19 17:37:47 +0900

    記事投稿機能を実装 (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getAuthUser } from "./lib/getAuthUser";
+import { PostLinkButton } from "./components/atoms/PostLinkButton/PostLinkButton";
 
 export default async function TopPage() {
   const authUser = await getAuthUser();
@@ -7,6 +8,10 @@ export default async function TopPage() {
     <>
       <h1 className="font-bold">トップページ</h1>
       <p>Welcome! {authUser ? authUser.name : "stranger"}</p>
+
+      <nav className="text-center my-2">
+        <PostLinkButton />
+      </nav>
     </>
   );
 }
---
+++ src/app/posts/create/page.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { getAuthUser } from "@/app/lib/getAuthUser";
+import { PostForm } from "@/app/components/organisms/PostForm";
+
+export const metadata: Metadata = {
+  title: "記事投稿フォーム - NextBlogApp",
+};
+
+export default async function PostCreatePage() {
+  const authUser = await getAuthUser();
+  if (!authUser) {
+    redirect("/login");
+  }
+
+  return (
+    <PostForm authUser = {authUser} />
+  );
+}
