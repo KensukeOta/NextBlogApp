9db1c5b45e92f89418cd1bf90d6bb52d0af5f6ec
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-05-22 01:07:34 +0900

    ログインページのボタンを上下左右中央に配置するようスタイリングを実装

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,25 +13,25 @@ export default async function Page() {
   }
   
   return (
-    <fieldset>
+    <div className="h-full flex flex-col items-center justify-center gap-2">
       {Object.values(providerMap).map((provider) => (
         <form
           action={async () => {
             "use server"
             await signIn(provider.id, { redirectTo: "/" })
           }}
-          className="flex justify-center"
+          className="flex items-center justify-center"
           key={provider.id}
         >
           <button
             type="submit"
-            className="flex items-center gap-2 mt-2 px-4 py-3 border rounded-lg hover:bg-blue-300"
+            className="flex items-center gap-2 px-4 py-3 border rounded-lg hover:bg-blue-300"
           >
             <i className={`bi bi-${provider.name.toLowerCase()} text-2xl`}></i>
             <span>{provider.name}でログインする</span>
           </button>
         </form>
       ))}
-    </fieldset>
+    </div>
   );
 }
