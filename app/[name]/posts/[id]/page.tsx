10351381d2eba33a2e7badca00dcd3c263d85754
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-05-23 01:46:24 +0900

    記事詳細表示機能を実装 (#6)

---
+++ app/[name]/posts/[id]/page.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import type { Post } from "@/app/types/Post";
+import { fetchPost } from "@/app/lib/data";
+
+export async function generateMetadata(
+  { params }: { params: { id: number } }
+): Promise<Metadata> {
+  const id = params.id;
+  const post: Post = await fetchPost(id);
+
+  return {
+    title: `${post.title}`,
+  };
+}
+
+export default async function Page({
+  params: { id, name }
+}: {
+  params: { id: number, name: string }
+}) {
+  const post: Post = await fetchPost(id);
+
+  return (
+    <section>
+      <h1 className="font-bold text-4xl">{post.title}</h1>
+      <p className="mt-1">by {post.user.name}</p>
+
+      <div className="mt-16">
+        <p className="whitespace-pre-wrap">{post.body}</p>
+      </div>
+    </section>
+  );
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,3 +21,25 @@ export async function fetchAllPosts() {
     console.log(error);
   }
 }
+
+export async function fetchPost(id: number) {
+  noStore();
+
+  try {
+    const res = await fetch(`${process.env.API_URL}/v1/api/posts/${id}`, {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      const errors = await res.json();
+      console.log(errors);
+      throw new Error(errors);
+    }
+    const data = await res.json();
+    return data.post;
+  } catch (error) {
+    console.log(error);
+  }
+}
