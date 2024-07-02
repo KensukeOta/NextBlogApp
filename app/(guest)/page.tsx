4e61e34e8a03db2374de4897846dcbf21a2c7120
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2024-07-02 18:58:11 +0900

    ページネーション機能を実装 (#18)

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@/auth";
+import { fetchPostsPages } from "@/app/lib/data";
 import { DefaultLayout } from "@/app/components/templates/DefaultLayout/DefaultLayout";
-import { Posts } from "../components/organisms/Posts";
+import { Posts } from "@/app/components/organisms/Posts";
+import { Pagination } from "../components/molecules/Pagination";
 
 export default async function Home({
   searchParams,
@@ -14,12 +16,18 @@ export default async function Home({
 
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchPostsPages(query) as number;
     
   return (
     <DefaultLayout className="py-6">
       <h1 className="mt-2">Welcome {session?.user ? session.user.name : "stranger"}</h1>
 
       <Posts query={query} currentPage={currentPage} />
+
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </DefaultLayout>
   );
 }
--- app/components/molecules/Search/Search.tsx
+++ app/components/molecules/Search/Search.tsx
@@ -17,6 +17,7 @@ export const Search = ({ placeholder }: { placeholder: string }) => {
 
   const handleSearch = (term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
