b22b8a8d9328dc94157cc8a59cfe0781c0c5d7cf
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-08-25 17:19:49 +0900

    ユーザーページのナビゲーションをスタイリング

--- app/components/molecules/PostNav/PostNav.tsx
+++ app/components/molecules/PostNav/PostNav.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import type { User } from "@/app/types/User";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const PostNav = ({ user }: {user: User}) => {
+export const PostNav = ({ user }: { user: User }) => {
+  const pathname = usePathname();
+  
   return (
     <nav className="mt-4">
-      <Link href={`/${user.name}`}>記事</Link>
-      <Link href={`/${user.name}/likes`}>いいね</Link>
+      <Link
+        href={`/${user.name}`}
+        className={`${pathname === `/${encodeURIComponent(user.name)}` && "bg-black text-white"} px-4 py-1 rounded-3xl`}
+      >
+        記事
+      </Link>
+      <Link
+        href={`/${user.name}/likes`}
+        className={`${pathname === `/${encodeURIComponent(user.name)}/likes` && "bg-black text-white"} px-4 py-1 rounded-3xl`}
+      >
+        いいね
+      </Link>
     </nav>
   );
 };
