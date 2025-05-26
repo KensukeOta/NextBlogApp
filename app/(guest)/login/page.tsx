262edd5faf204d454442549a75fef79992ab04b9
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-05-26 19:43:27 +0900

    htmlタグのlang属性をjaに変更し、descriptionを記述

---
+++ app/(guest)/login/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ログイン",
+};
+
+export default function LoginPage() {
+  return <h1>ログイン</h1>;
+}
---
+++ app/(guest)/signup/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "サインアップ",
+};
+
+export default function LoginPage() {
+  return <h1>サインアップ</h1>;
+}
