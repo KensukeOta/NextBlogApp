1a83f752fddffc3c74725dc19be71ffa7c1b6e77
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-05-28 19:00:30 +0900

    ログアウト処理中はdisabled属性をtrueに設定し、二重送信防止

--- app/components/atoms/LogoutButton/LogoutButton.tsx
+++ app/components/atoms/LogoutButton/LogoutButton.tsx
@@ -1,10 +1,19 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useState } from "react";
 
 export const LogoutButton = () => {
+  const [isLoding, setIsLoading] = useState(false);
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    await signOut({ redirectTo: "/login" });
+    setIsLoading(false);
+  };
+
   return (
-    <button type="submit" onClick={() => signOut({ redirectTo: "/login" })}>
+    <button type="submit" onClick={handleLogout} disabled={isLoding}>
       ログアウト
     </button>
   );
