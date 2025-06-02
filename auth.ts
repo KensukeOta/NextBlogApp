863ddfd6b68752650cbf9884d1f291d261e9a768
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-06-02 12:04:50 +0900

    OAuth認証後にユーザー登録が行われるよう実装

--- auth.ts
+++ auth.ts
@@ -46,6 +46,54 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
 
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "credentials") {
+        return true;
+      }
+
+      const name = user.name;
+      const email = user.email;
+      const image = user.image;
+      const provider = account?.provider;
+
+      try {
+        const res = await fetch(`${process.env.API_URL}/v1/oauth`, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user: {
+              name,
+              email,
+              provider,
+              image,
+            },
+          }),
+        });
+        if (!res.ok) {
+          const errors = await res.json();
+          throw new Error(errors);
+        } else {
+          // APIサーバー側から正しいユーザーデータを受け取る
+          const data = await res.json();
+          const registeredUser = data.user;
+
+          // セッションのuser情報をここで上書きして返す
+          user.name = registeredUser.name;
+          user.email = registeredUser.email;
+          user.image = registeredUser.image;
+          user.id = registeredUser.id;
+          return true;
+        }
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
   pages: {
     signIn: "/login",
   },
