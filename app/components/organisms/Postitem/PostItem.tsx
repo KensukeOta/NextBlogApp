a41fa5c1b0dceee021faf5b430c7e4b7d90285cc
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2025-06-03 18:20:15 +0900

    記事一覧表示機能を実装 (#8)

---
+++ app/components/organisms/Postitem/PostItem.tsx
@@ -0,0 +1,21 @@
+import { Post } from "@/app/types/Post";
+import Link from "next/link";
+
+export const PostItem = ({ post }: { post: Post }) => {
+  return (
+    <article className="border bg-white p-2">
+      <h2 className="text-left font-bold">
+        <Link
+          href={`/${post.user.name}/posts/${post.id}`}
+          className="inline-block w-full hover:underline"
+        >
+          {post.title}
+        </Link>
+      </h2>
+
+      <nav className="flex justify-between">
+        <p>by {post.user.name}</p>
+      </nav>
+    </article>
+  );
+};
---
+++ app/lib/data.ts
@@ -0,0 +1,19 @@
+export async function fetchAllPosts() {
+  try {
+    const res = await fetch(`${process.env.API_URL}/v1/posts`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      const errors = await res.json();
+      console.log(errors);
+      throw new Error(errors);
+    }
+    const data = await res.json();
+    return data.posts;
+  } catch (error) {
+    console.log(error);
+  }
+}
