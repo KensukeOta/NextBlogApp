5c5501cc1bd115b063276ae7b6f7a0524bf90160
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-06-03 18:18:55 +0900

    記事一覧表示機能を実装 (#7)

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,14 +1,26 @@
-import Link from "next/link";
+import type { Post } from "../types/Post";
 import { auth } from "@/auth";
+import { PostItem } from "../components/organisms/Postitem";
+import { DefaultLayout } from "../components/templates/DefaultLayout";
+import { fetchAllPosts } from "../lib/data";
 
 export default async function Page() {
   const session = await auth();
 
+  const posts: Post[] = await fetchAllPosts();
+
+  const postItems =
+    posts.length > 0 ? (
+      posts.map((post) => <PostItem key={post.id} post={post} />)
+    ) : (
+      <p className="text-center font-bold">記事が投稿されていません</p>
+    );
+
   return (
-    <div>
+    <DefaultLayout className="py-6">
       <p>Hello, {session?.user ? session.user.name : "stranger"}</p>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
+
+      <section className="mt-2">{postItems}</section>
+    </DefaultLayout>
   );
 }
