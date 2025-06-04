8c9b446d0a47e096f333af85461c7b01c6e0b57f
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-06-04 13:15:08 +0900

    記事更新機能を実装 (#11)

--- app/components/organisms/PostEditForm/PostEditForm.tsx
+++ app/components/organisms/PostEditForm/PostEditForm.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import type { Post } from "@/app/types/Post";
-import { useState } from "react";
+import { useActionState, useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "github-markdown-css/github-markdown.css";
+import { PostState, updatePost } from "@/app/lib/actions";
 
 export const PostEditForm = ({ post }: { post: Post }) => {
+  const initialState: PostState = { message: null, errors: {}, values: {} };
+  const updatePosteWithId = updatePost.bind(null, post.id);
+  const [state, formAction, isPending] = useActionState(updatePosteWithId, initialState);
+
   const [title, setTitle] = useState(post.title);
   const [content, setContent] = useState(post.content);
 
@@ -17,7 +22,19 @@ export const PostEditForm = ({ post }: { post: Post }) => {
     };
 
   return (
-    <form className="h-full">
+    <form action={formAction} className="h-full">
+      <div id="edit-error" aria-live="polite" aria-atomic="true">
+        {state?.message && <p className="text-red-500">{state.message}</p>}
+      </div>
+
+      <div id="title-error" aria-live="polite" aria-atomic="true">
+        {state?.errors?.title &&
+          state.errors.title.map((error: string) => (
+            <p className="text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
+      </div>
       <input
         type="text"
         name="title"
@@ -31,6 +48,14 @@ export const PostEditForm = ({ post }: { post: Post }) => {
       />
 
       <div className="mt-2 h-[calc(100%-6.125rem)]">
+        <div id="content-error" aria-live="polite" aria-atomic="true">
+          {state?.errors?.content &&
+            state.errors.content.map((error: string) => (
+              <p className="text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+        </div>
         <div className="flex h-full">
           <textarea
             name="content"
@@ -49,7 +74,11 @@ export const PostEditForm = ({ post }: { post: Post }) => {
         </div>
       </div>
 
-      <button type="submit" className="mt-2 w-full bg-black p-2 text-white hover:opacity-70">
+      <button
+        type="submit"
+        disabled={isPending}
+        className="mt-2 w-full bg-black p-2 text-white hover:opacity-70"
+      >
         更新する
       </button>
     </form>
