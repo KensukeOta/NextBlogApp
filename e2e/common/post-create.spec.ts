ae44069d27e9837baa1e03985c69f9d6f5a745ed
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-07-11 06:24:04 +0900

    記事投稿の際と記事更新の際に、タグも紐づけて送信するE2Eテストを実装

--- e2e/common/post-create.spec.ts
+++ e2e/common/post-create.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from "@playwright/test";
 import { login } from "../helpers/login";
-
 // 有効なタイトルと本文で記事投稿できる
 test("can post create and see top page", async ({ page }) => {
   // ログイン処理
@@ -20,6 +19,30 @@ test("can post create and see top page", async ({ page }) => {
   await page.getByRole("button", { name: "投稿する" }).click();
   await expect(page).toHaveURL("/");
   await expect(page.getByRole("link", { name: title })).toBeVisible();
+});
+
+// 有効なタイトルと本文とタグで記事投稿できる
+test("can post and tag create and see top page", async ({ page }) => {
+  // ログイン処理
+  await login(
+    page,
+    process.env.TEST_USER_EMAIL as string,
+    process.env.TEST_USER_PASSWORD as string,
+  );
+
+  const title = Array.from({ length: 5 }, () => Math.random().toString(36)[2]).join("");
+  const content = Array.from({ length: 100 }, () => Math.random().toString(36)[2]).join("");
+  const tagName = Array.from({ length: 5 }, () => Math.random().toString(36)[2]).join("");
+
+  await page.goto("/posts/create");
+  await page.getByRole("textbox", { name: "タイトル" }).fill(title);
+  await page.getByTestId("input").fill(tagName);
+  await page.getByTestId("input").press("Enter");
+  await page.getByRole("textbox", { name: "本文" }).fill(content);
+  await page.getByRole("button", { name: "投稿する" }).click();
+  await expect(page).toHaveURL("/");
+  await expect(page.getByRole("link", { name: title })).toBeVisible();
+  await expect(page.getByRole("link", { name: tagName })).toBeVisible();
 });
 
 // 最低文字数以下で入力した場合、バリデーションメッセージが表示される
--- e2e/common/post-edit.spec.ts
+++ e2e/common/post-edit.spec.ts
@@ -19,7 +19,8 @@ test("can post edit and see top page", async ({ page }) => {
 
   const editTitle = Array.from({ length: 5 }, () => Math.random().toString(36)[2]).join("");
   const editContent = Array.from({ length: 100 }, () => Math.random().toString(36)[2]).join("");
-  await page.goto("http://localhost:3000/");
+
+  await page.goto("/");
   await page
     .getByRole("article")
     .filter({ hasText: `${title}` })
@@ -31,6 +32,41 @@ test("can post edit and see top page", async ({ page }) => {
   await page.getByRole("button", { name: "更新する" }).click();
   await expect(page).toHaveURL("/");
   await expect(page.getByRole("link", { name: editTitle })).toBeVisible();
+});
+
+// 有効なタイトルと本文とタグで記事の更新ができる
+test("can post and tag edit and see top page", async ({ page }) => {
+  // ログイン処理
+  await login(
+    page,
+    process.env.TEST_USER_EMAIL as string,
+    process.env.TEST_USER_PASSWORD as string,
+  );
+
+  // 記事作成処理
+  const title = Array.from({ length: 5 }, () => Math.random().toString(36)[2]).join("");
+  const content = Array.from({ length: 100 }, () => Math.random().toString(36)[2]).join("");
+
+  await postCreate(page, title, content);
+
+  const editTitle = Array.from({ length: 5 }, () => Math.random().toString(36)[2]).join("");
+  const editContent = Array.from({ length: 100 }, () => Math.random().toString(36)[2]).join("");
+  const tagName = Array.from({ length: 5 }, () => Math.random().toString(36)[2]).join("");
+
+  await page.goto("/");
+  await page
+    .getByRole("article")
+    .filter({ hasText: `${title}` })
+    .getByRole("link", { name: "更新" })
+    .click();
+  await page.getByRole("textbox", { name: "タイトル" }).fill(editTitle);
+  await page.getByTestId("input").fill(tagName);
+  await page.getByTestId("input").press("Enter");
+  await page.getByRole("textbox", { name: "本文" }).fill(editContent);
+  await page.getByRole("button", { name: "更新する" }).click();
+  await expect(page).toHaveURL("/");
+  await expect(page.getByRole("link", { name: editTitle })).toBeVisible();
+  await expect(page.getByRole("link", { name: tagName })).toBeVisible();
 });
 
 // 未ログインユーザが記事更新ページに移動すると、トップページにリダイレクトされる
